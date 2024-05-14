dd477b49d0bb752bb3af32079800fd602b341673
Tztedtrzh <120391182+BenChanlLOL@users.noreply.github.com>
2024-05-14 18:14:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # main.py
 # Author: "BenChanlLOL" on github
 # Name: ConCat
-# Version: 0.2
+# Version: 0.2.3
 # link to github project: https://github.com/BenChanlLOL/ConCat
-# updated on 14/05/2024
+# updated on 14/06/2024
 
 import socket
 from sys import argv
@@ -17,6 +17,7 @@ if len(argv) == 2:
     sys.exit()
 
 
+
 print("Welcome to ConCat")
 num1 = random.randint(0, random.randint(0, 2048))
 num2 = random.randint(0, random.randint(0, 2048))
@@ -27,10 +28,12 @@ print("session code is:", session_code)
 #pre defined variables
 sock = socket.socket()
 serverOn = False
+AllowBind = False
 
 while True:
     address1 = input("what ip should we connect to?    ")
     if address1 == "ignore":
+        AllowBind = True
         break
     try:
         port1 = int(input("what port should we connect to?    "))
@@ -51,43 +54,42 @@ while True:
     cmd = input(">>>  ")
     if cmd == "exit":
         break
-    elif cmd == "git-open":
+    if cmd == "git-open":
         print("opening github")
         webbrowser.open('https://github.com/BenChanlLOL/ConCat')
         print("done")
-    elif cmd == "bind":
-        try:
+    if cmd == "bind":
+        if AllowBind == True:
             for n in range(0,6):
-                print(n)
-                time.sleep(1)
+                 print(n)
             try:
+                time.sleep(1)
                 sock.bind((socket.gethostname(), 7092))
                 print("code 0")
             finally:
-                    serverOn = True
-                    sock.listen(5)
-                    print("listening")
-                    conn, addr = sock.accept()
+                serverOn = True
+                sock.listen(5)
+                print("listening")
+                conn, addr = sock.accept()
             if sock.accept:
                 print(conn, addr)
             print("starting on port 7092")
             print("server started")
-        except OSError as e:
-            print("Error occurred while binding socket: " + str(e))
-            print("\n")
-            print("perhaps run the program as sudo")
-            print("or restart the program and, when prompted for a ip and port to connect to say 'ignore'")
-        while True:
+          
             msg = conn.recv(1024).decode("utf-8")
             print(msg)
-    elif cmd == "status":
+        if AllowBind == False:
+            print("you dont have permission to bind")
+            print("OR you did not input 'ignore' when prompted for an IP to connect to")
+            break
+    if cmd == "status":
         if serverOn:
             print("server is running")
             print("All services are succesful")
             print("CODE: 0")
         else:
             print("server is not running")
-    elif cmd == "connect":
+    if cmd == "connect":
         while True:
             print("client started or message sent")
             msg = input(">>>  ")
@@ -109,9 +111,10 @@ while True:
         '''
 
         print("usage:\n" + usage)
-    elif cmd == "version" or "-v":
-        print("version: 0.1")
-    elif cmd == "troubleshoot":
+        break
+    if cmd == "version" or "-v":
+        print("version: 0.2.3")
+    if cmd == "troubleshoot":
         print("If you are attempting to ssh using this client a BrokenPipeError is a indicator of wrong password"
               "When Using SSH a small delay after the first message will occur, then echo the SSH version. "
               "Then you will have three opportunities to enter a password"
@@ -121,3 +124,4 @@ while True:
 
     else:
         print('use "help" to get info')
+    break
